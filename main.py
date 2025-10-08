fc9ca265937b528441287880b396853d9f8db863
Ferboinkles <vihaan4nasa@gmail.com>
2025-10-08 18:13:28 +0100

    Added background

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import pygame
 import sys
 
-# Start pygame
 pygame.init()
-
-# Create a window (800x600)
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Pusheen!")
-
+background = pygame.transform.scale(pygame.image.load("background.png"), (1000, 800))
 # Main loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             sys.exit()
-
-    # Fill background color
-    window.fill((173, 216, 230))  # light blue
-
-    # Draw a "desktop icon"
-    pygame.draw.rect(window, (255, 255, 255), (100, 100, 80, 80))
-    pygame.draw.circle(window, (0, 0, 255), (140, 140), 30)
-
+    window.blit(background, (0, 0))
     # Update display
     pygame.display.flip()
+
+   # print("Sleepy time!")
+    #sys.exit()  # Immediately stops the program
+
