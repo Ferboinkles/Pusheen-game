10189f96166627b0a0e77b81948d195dc0468fc2
Ferboinkles <vihaan4nasa@gmail.com>
2025-10-11 22:05:54 +0100

    Added the second pipe

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import sys
 
 import pygame
 
+# Variabllllllllllles! Awwwwwwwww yeah!
 pygame.init()
 window = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Pusheen!")
 background = pygame.transform.scale(pygame.image.load("background.png"), (1000, 800))
 pusheen = pygame.transform.scale(pygame.image.load("pusheen.png"), (200, 100))
 pipe = pygame.transform.scale(pygame.image.load("pipe.png"), (86, 300))
+pipe2 = pygame.transform.scale(pygame.image.load("pipe2.png"), (86, 300))
 pipeX = 1000.0
 pipeY = 540.0
+pipe2X = 1000.0
+pipe2Y = 0.0
+pusheenX = 50.0
 pusheenY = 350.0
+#Main game
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     window.blit(background, (0, 0))
     window.blit(pipe, (pipeX, pipeY))
+    window.blit(pipe2, (pipe2X, pipe2Y))
     pipeX -= 3
+    pipe2X -= 3
     keys = pygame.key.get_pressed()
     pusheenY += 4
     if keys[pygame.K_SPACE]:
         pusheenY -= 7
-    window.blit(pusheen, (0, pusheenY))
+    window.blit(pusheen, (pusheenX, pusheenY))
 
     pygame.display.flip()
