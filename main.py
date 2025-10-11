5319aee632d73912f9d62c88688ea6c495d6ee50
Ferboinkles <vihaan4nasa@gmail.com>
2025-10-11 14:58:16 +0100

    Added sprite and space bar functionality

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import pygame
 import sys
+
+import pygame
 
 pygame.init()
 window = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Pusheen!")
 background = pygame.transform.scale(pygame.image.load("background.png"), (1000, 800))
-# Main loop
+pusheen = pygame.transform.scale(pygame.image.load("pusheen.png"), (200, 100))
+pusheenX = 0.0
+pusheenY = 350.0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     window.blit(background, (0, 0))
-    # Update display
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        pusheenY -= 2
+    window.blit(pusheen, (pusheenX, pusheenY))
+
     pygame.display.flip()
-
-   # print("Sleepy time!")
-    #sys.exit()  # Immediately stops the program
-
