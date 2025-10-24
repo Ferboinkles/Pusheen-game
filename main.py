e40a6ac65b3adbc96b7f9d11d5da991d8e1d9d8b
Ferboinkles <vihaan4nasa@gmail.com>
2025-10-24 18:00:27 +0100

    Changed text colour and added a start countdown

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ window = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Pusheen!")
 score = 0
 clock = pygame.time.Clock()
+startDown = True
 background = pygame.transform.scale(pygame.image.load("background.png"), (1000, 800))
 pusheen = pygame.transform.scale(pygame.image.load("pusheen.png"), (150, 100))
 pipe = pygame.transform.scale(pygame.image.load("pipe.png"), (150, 700))
@@ -28,11 +29,11 @@ font = pygame.font.SysFont("javanesetext", 70)
 pipeX = 1000.0
 pipeY = random.randint(200, 700)
 pipe2X = 1000.0
-pipe2Y = pipeY - 900
+pipe2Y = pipeY - 1000
 pipe3X = 500.0
 pipe3Y = random.randint(200, 700)
 pipe4X = 500.0
-pipe4Y = pipe3Y - 900
+pipe4Y = pipe3Y - 1000
 pusheenX = 50.0
 pusheenY = 350.0
 # Rectangles
@@ -50,9 +51,10 @@ while True:
     window.blit(background, (0, 0))
     if pygame.Rect.colliderect(pipeRect, pusheenRect) or pygame.Rect.colliderect(pipe2Rect,
                                                                                  pusheenRect) or pygame.Rect.colliderect(
-        pipe3Rect, pusheenRect) or pygame.Rect.colliderect(pipe4Rect, pusheenRect):
-        gameOverText = font.render("Pusheen fell asleep!", 1, (39, 2, 222))
-        gameOverText2 = font.render(f"You got a score of {str(score)}", 1, (39, 2, 222))
+        pipe3Rect, pusheenRect) or pygame.Rect.colliderect(pipe4Rect,
+                                                           pusheenRect) or pusheenY == 10 or pusheenY == -810:
+        gameOverText = font.render("Pusheen fell asleep!", 1, (10, 208, 247))
+        gameOverText2 = font.render(f"You got a score of {str(score)}", 1, (10, 208, 247))
         window.blit(gameOverText, (200, 300))
         window.blit(gameOverText2, (200, 400))
         pygame.display.update()
@@ -62,7 +64,7 @@ while True:
         score = score + 1
     if pipe4X == 100:
         score = score + 1
-    scoreText = font.render(str(score), 1, "blue")
+    scoreText = font.render(str(score), 1, (10, 208, 247))
     window.blit(scoreText, scoreText.get_rect(centerx=100))
     pipeRect.update(pipeX, pipeY, 150, 700)
     pipe2Rect.update(pipe2X, pipe2Y, 150, 700)
@@ -81,24 +83,40 @@ while True:
         pipeX = 1000.0
         pipeY = random.randint(200, 700)
         pipe2X = 1000.0
-        pipe2Y = pipeY - 900
+        pipe2Y = pipeY - 1000
 
     if pipe3X <= -100:
         pipe3X = 1000.0
         pipe3Y = random.randint(200, 700)
         pipe4X = 1000.0
-        pipe4Y = pipe3Y - 900
+        pipe4Y = pipe3Y - 1000
 
     keys = pygame.key.get_pressed()
-    pusheenY += 3.5
+    pusheenY += 4.5
     if keys[pygame.K_SPACE]:
-        pusheenY -= 8
+        pusheenY -= 9
     window.blit(pusheen, (pusheenX, pusheenY))
-    pygame.display.flip()
     if keys[pygame.K_b]:
-        gameOverText = font.render("You seem to have pressed the paws button!", 1, (39, 2, 222))
-        gameOverText2 = font.render("Have a 5 second break meow!", 1, (39, 2, 222))
+        gameOverText = font.render("You seem to have pressed the paws button!", 1, (10, 208, 247))
+        gameOverText2 = font.render("Have a 5 second break meow!", 1, (10, 208, 247))
         window.blit(gameOverText, (0, 300))
         window.blit(gameOverText2, (80, 400))
         pygame.display.update()
         time.sleep(5)
+    pygame.display.flip()
+    if startDown:
+        text3 = font.render("3", 1, (10, 208, 247))
+        window.blit(text3, (500, 300))
+        pygame.display.update()
+        time.sleep(1)
+        text2 = font.render("2", 1, (10, 208, 247))
+        window.blit(text2, (550, 300))
+        pygame.display.update()
+        time.sleep(1)
+        text1 = font.render("1", 1, (
+            10, 208, 247
+        ))
+        window.blit(text1, (600, 300))
+        pygame.display.update()
+        time.sleep(1)
+        startDown = False
